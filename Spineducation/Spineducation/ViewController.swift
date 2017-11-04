e2ab5756cd38b95c2759088854386b308eacd1a6
Maya Ramamurthy <mayaramamurthy@Mayas-MacBook-Pro-2.local>
2017-11-04 16:46:01 -0400

    Issue #29 - fixed so spine shows up with right orientation and if click is not valid, offers alternative spot.

--- Spineducation/Spineducation/ViewController.swift
+++ Spineducation/Spineducation/ViewController.swift
@@ -16,7 +16,10 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     var nodeModel:SCNNode!
     let nodeName = "l4_Default" // Same name we set for the node on SceneKit's editor
     var spineExists = false;
+    
 
+    let spine = SCNScene(named: "art.scnassets/spine-collection-of-thunthu/17_10_25_1909.dae")! // sets the spine to spine 3d image file
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,11 +36,18 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Create a new scene
         //let scene = SCNScene(named: "art.scnassets/ship.scn")!
         
+        
         let scene = SCNScene()
-
+        nodeModel =  scene.rootNode.childNode(withName: nodeName, recursively: true)
+    //    spine.rootNode.position = SCNVector3Make(1, 0,1)
+        sceneView.scene.rootNode.addChildNode(spine.rootNode) // add this to the sc
+        
+        self.sceneView.autoenablesDefaultLighting = true
+        self.sceneView.antialiasingMode = .multisampling4X
         
         // Set the scene to the view
         sceneView.scene = scene
+        
     }
     
 
@@ -50,6 +60,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
         // Run the view's session
         sceneView.session.run(configuration)
+        
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -60,22 +71,52 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        guard let touch = touches.first else {return}
+      guard let touch = touches.first else {return}
         let result = sceneView.hitTest(touch.location(in: sceneView), types: ARHitTestResult.ResultType.featurePoint)
+        
+        // call on didTap method to draw the spine if the position has been clicked
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.didTap(withGestureRecognizer:)))
+        sceneView.addGestureRecognizer(tapGestureRecognizer)
+    /*
         guard let hitResult = result.last else {return}
         let hitTransform = SCNMatrix4(hitResult.worldTransform) // make SCNMatrix4 object out of touch location
         let hitVector = SCNVector3Make(hitTransform.m41, hitTransform.m42, hitTransform.m43) // take relevant components of the touch location from matrix to make SVNVector3 object
+        
         if (!spineExists){ // if the spine is not on the screen yet
             createSpine(position: hitVector) // create the spine
             spineExists = true // update the variable to reflect that the spine exists now
+        }*/
+    }
+    
+    @objc func didTap(withGestureRecognizer recognizer: UIGestureRecognizer) {
+        let tapLocation = recognizer.location(in: sceneView)
+        let hitTestResults = sceneView.hitTest(tapLocation)
+        let result = sceneView.hitTest(tapLocation, types: ARHitTestResult.ResultType.featurePoint)
+        //guard
+        let hitResult = result.last //else {return}
+        guard let node = hitTestResults.first?.node else {
+            let hitTestResultsWithFeaturePoints = sceneView.hitTest(tapLocation, types: .featurePoint)
+            if  (hitTestResultsWithFeaturePoints.first != nil) {
+                let hitTransform = SCNMatrix4((hitResult?.worldTransform)!) // make SCNMatrix4 object out of touch location
+                let hitVector = SCNVector3Make(hitTransform.m41, hitTransform.m42, hitTransform.m43) // take relevant components of the touch location
+                createSpine(position: hitVector)
+            }
+            else {
+                createSpine(position: SCNVector3Make(1,0,-1))
+            }
+            return
         }
+        spine.rootNode.removeFromParentNode()
     }
     
     func createSpine(position : SCNVector3){
-        let spine = SCNScene(named: "art.scnassets/spine-collection-of-thunthu/17_10_25_1909.dae")! // sets the spine to spine 3d image file
-        nodeModel =  spine.rootNode.childNode( // recursively binds child nodes to the root node
-            withName: nodeName, recursively: true)
-        spine.rootNode.position = position // sets the position of the root node to the position from the SVNVector3 object
+        nodeModel =  spine.rootNode.childNode( withName: nodeName, recursively: true)// recursively binds child node to root node
+        spine.rootNode.position = SCNVector3Make(position.x,position.y + 0.5,position.z) // sets the position of root node
+ //      spine.rootNode.position = position // sets the position of the root node to the position from the SVNVector3 object
+        let camera = self.sceneView.pointOfView!
+        
+   //     spine.rootNode.position = camera.convertPosition(position, to: nil)
+        spine.rootNode.rotation = camera.rotation
         sceneView.scene.rootNode.addChildNode(spine.rootNode) // add this to the scene
     }
     
@@ -114,18 +155,53 @@ class ViewController: UIViewController, ARSCNViewDelegate {
      Called when a SceneKit node corresponding to a
      new AR anchor has been added to the scene.
      */
-    func renderer(_ renderer: SCNSceneRenderer,
-                  didAdd node: SCNNode, for anchor: ARAnchor) {
-        // ...
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        // Place content only for anchors found by plane detection.
+        guard let planeAnchor = anchor as? ARPlaneAnchor else { return }
+        
+        // Create a SceneKit plane to visualize the plane anchor using its position and extent.
+        let plane = SCNPlane(width: CGFloat(planeAnchor.extent.x), height: CGFloat(planeAnchor.extent.z))
+        let planeNode = SCNNode(geometry: plane)
+        planeNode.simdPosition = float3(planeAnchor.center.x, 0, planeAnchor.center.z)
+        
+        /*
+         `SCNPlane` is vertically oriented in its local coordinate space, so
+         rotate the plane to match the horizontal orientation of `ARPlaneAnchor`.
+         */
+        planeNode.eulerAngles.x = -.pi / 2
+        
+        // Make the plane visualization semitransparent to clearly show real-world placement.
+        planeNode.opacity = 0.25
+        
+        /*
+         Add the plane visualization to the ARKit-managed node so that it tracks
+         changes in the plane anchor as plane estimation continues.
+         */
+        node.addChildNode(planeNode)
     }
     
     /*
      Called when a SceneKit node's properties have been
      updated to match the current state of its corresponding anchor.
      */
-    func renderer(_ renderer: SCNSceneRenderer,
-                  didUpdate node: SCNNode, for anchor: ARAnchor) {
-        // ...
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        // Update content only for plane anchors and nodes matching the setup created in `renderer(_:didAdd:for:)`.
+        guard let planeAnchor = anchor as?  ARPlaneAnchor,
+            let planeNode = node.childNodes.first,
+            let plane = planeNode.geometry as? SCNPlane
+            else { return }
+        
+        // Plane estimation may shift the center of a plane relative to its anchor's transform.
+        planeNode.simdPosition = float3(planeAnchor.center.x, 0, planeAnchor.center.z)
+        
+        /*
+         Plane estimation may extend the size of the plane, or combine previously detected
+         planes into a larger one. In the latter case, `ARSCNView` automatically deletes the
+         corresponding node for one plane, then calls this method to update the size of
+         the remaining plane.
+         */
+        plane.width = CGFloat(planeAnchor.extent.x)
+        plane.height = CGFloat(planeAnchor.extent.z)
     }
     
     /*
@@ -136,6 +212,5 @@ class ViewController: UIViewController, ARSCNViewDelegate {
                   didRemove node: SCNNode, for anchor: ARAnchor) {
         // ...
     }
-    
     
 }
