82b9f3a6f76db07bb4f953e1d8e6514eaaab4066
Randa <randamohsen@hotmail.ca>
2017-11-05 11:26:37 -0500

    Merging #4 and #5
    Created a main page that allows the user to choose between the spine and the cases

---
+++ Spineducation/Spineducation/TemporaryMainPageVC.swift
@@ -0,0 +1,24 @@
+//
+//  SimpleMainViewController.swift
+//  SpinePHPtesting
+//
+//  Created by Randa Mohsen on 2017-11-04.
+//  Copyright © 2017 Spineducation. All rights reserved.
+//
+
+import UIKit
+
+class TemporaryMainPageVC: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    
+}
+
